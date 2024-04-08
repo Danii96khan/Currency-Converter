4c16b19a96ccaa4862f5efbde8ac1b954da3d7a4
Daniyal Khan <159725508+Danii96khan@users.noreply.github.com>
2024-04-08 07:51:07 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+//In PKR
+
+let conversion = {
+    "PKR": {
+        "USD":0.036,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+//In GBP
+
+    "GBP": {
+        "USD":1.21,
+        "PKR":350,
+        "GBP":1
+    },
+
+    //In USD
+    "USD": {
+        "GBP":0.83,
+        "PKR":227.58,
+        "USD":1
+    }
+}
+
+//Step 2 using inquirer
+const answer: {
+    from: "PKR" | "USD" | "GBP",
+ to: "PKR" | "USD" | "GBP",
+
+ //datatype declared
+ amount: number
+} = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        message: "Select your currency?",
+        choices: ["PKR", "USD", "GBP"],
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "Select your conversion currency?",
+        choices: ["PKR", "USD", "GBP"],
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter the amount you want to convert",
+    }
+])
+
+//step 3 
+
+const {from,to,amount}=answer;
+ 
+// check input
+
+if (from && to && amount){
+    let result = conversion[from][to] * amount
+    console.log(`your conversion from ${from} to  ${to} is ${result}`)
+}else {
+    console.log("invalid inputs")
+}
